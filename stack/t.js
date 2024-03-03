1419c20df321d57b8ef6df67666dd9611156706a
james_ma <t90laser93@gmail.com>
2024-03-03 14:36:16 +0900

    조합 - 인프런 강의 참고 - 백트래킹을 구현하기 위해서 재귀 활용, 특정 포인트로 돌아간다는게 어떤 의미인지 이해 - stack을 활용해도 되고, 정해진 길이의 배열을 사용해도 된다는 점

--- stack/t.js
+++ stack/t.js
@@ -40,3 +40,53 @@ permute([1, 2, 3, 4]);
 // 그리고 다시 2로 돌아가서 이번에는 4 - 3 순서대로 처리한다.
 
 // 이게 가장 일반적인 형태의 로직인데 이걸 how to implement in code?
+
+// 스택에 밀어넣고 pop()하고 이러면서 구현할 건덕지가 분명히 있어보이는데.....그게 뭘까...
+
+// 반복문은 필요해 시작 지점을 잡기 위해서.
+// 스택에 1을 넣는다.
+// 그 다음에 재귀적으로 그 다음 순서를 실행되도록 한다.
+// 이게 맞나?무엇을 push pop할지 어떻게 알지
+//
+
+// 아이디어가 떠오르지 않아서 인프런 강의를 들었음.
+// 우선 구두 설명을 듣고 나서 먼저 구현을 시도하겠음
+
+// 주어진 배열 중 N개로 구성된 순열을 다 반환하는 로직
+function infPermute(N, arr) {
+  const answer = [];
+
+  const isUsed = Array.from({ length: arr.length }, () => 0);
+  const selected = Array.from({ length: N }, () => 0);
+
+  const stack = [];
+
+  // L - 처리되는 선택 순서
+  function dfs(L) {
+    if (L === N) {
+      answer.push([...selected]);
+    } else {
+      for (let i = 0; i < arr.length; i++) {
+        // 사용할 수 있는지 확인
+        if (isUsed[i] === 0) {
+          isUsed[i] = 1;
+          // selected[L] = arr[i]; // 사용 가능한 상태인 값을 selected배열에 더한다.
+          // stack으로 해도 되겠지.
+          stack.push(arr[i]);
+          // 재귀 호출
+          dfs(L + 1);
+          // 여기서부터가 이전 분기에서 돌아온 BACK시점. 즉 사용 취소 처리를 해줘야 한다.
+          isUsed[i] = 0;
+          stack.pop();
+        }
+      }
+    }
+  }
+  dfs(0);
+
+  return answer;
+}
+
+console.log(infPermute(2, [1, 2, 3]));
+
+// 구조적으로 머릿속으로 조금 더 생각해보면서 이해가 되었다.
