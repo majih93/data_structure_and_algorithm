2a1d8892db13e56e2a2227962d506f866da9b7a5
james_ma <t90laser93@gmail.com>
2024-03-05 21:55:14 +0900

    프로그래머스 - 숫자 문자열과 영단어(다른 사람 풀이 참고) - split()함수와 join() 함수의 깔끔한 사용

--- programmers/숫자 문자열과 영단어.js
+++ programmers/숫자 문자열과 영단어.js
@@ -43,3 +43,41 @@ function solution(s) {
 }
 
 console.log(solution("one4seveneight"));
+
+// 배울 점이 있는 다른 사람의 코드
+
+function solution_other(s) {
+  const numbers = [
+    "zero",
+    "one",
+    "two",
+    "three",
+    "four",
+    "five",
+    "six",
+    "seven",
+    "eight",
+    "nine",
+  ];
+
+  let answer = s;
+
+  // numbers배열의 값들에 대해, 한 번씩 돌면서 해당 문자열로 문자를 나눈 뒤, 숫자로 대체해서 다시 join한다.
+
+  // "one4seveneight"
+  // ["", "4seveneight"] split하면 (one으로)
+  // one에 해당되는 인덱스값 1로 join 하면 -> "14seveneight"
+  // 이걸 모든 배열 값에 대해서 반복 후에
+  // 합쳐서 number로 변환
+
+  // split과 join의 특성을 이용한게 포인트
+  // 함수를 잘 이해하고 쓰면 이게 되는구나.
+  for (let i = 0; i < numbers.length; i++) {
+    let arr = answer.split(numbers[i]);
+    answer = arr.join(i);
+  }
+
+  return Number(answer);
+}
+
+console.log("one4seven".split("one"));
